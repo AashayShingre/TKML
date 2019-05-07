1e55decafaa3d23fe2b49a25b8926d2ac323026f
Aashay Shingre <akshingre@gmail.com>
2019-05-07 15:00:26 +0530

    test-2 edit

--- test-2.py
+++ test-2.py
@@ -7,7 +7,7 @@ a = TKML(tkmlfile)
 builtWindow = a.getBuiltWindow()
 
 button_tags = a.getTag('Button') ##returns a list of all tkinter button instances
-name_class = a.getClass('name') ##returns a list of all tkinter instances with class = name
-equal_button = a.getID('id') ##return a list of all tkinter instance with id = id
+
+button_tags[0]['text'] = "Press"
 
 builtWindow.mainloop()
